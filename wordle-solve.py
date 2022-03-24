8804056349f0c6d073c65ce5144a7753e76e6d50
Mert Oğuzhanasiltürk <mert.oguzhanasilturk@gmail.com>
2022-03-25 00:11:59 +0300

    yanlış yerlere göre işlem yapabiliyor

--- wordle-solve.py
+++ wordle-solve.py
@@ -1,5 +1,6 @@
 from tkinter.filedialog import Open
 import enDegerliKelimeler as deger
+import json
 
 def start():
     with open("Dogru-Yerde-Harfler.txt","w",encoding="utf-8") as f:
@@ -18,7 +19,7 @@ def start():
 def cozum():
     girdi=input("girdiniz ne oldu: ")
     print("Doğru yerdekiler için *\nYanlış yerdekiler için +\nOlmayanlar için -")
-
+    sira=0
     for i in girdi:
         donus=input(f"{i} harfi geri dönüsü: ")
         if donus=="*":
@@ -27,13 +28,14 @@ def cozum():
                 f.close()
         elif donus=="+":
             with open("Olan-Harfler.txt","a",encoding="utf-8") as f:
-                f.write(f"{i}\n")
+                f.write(f"{i},{str(sira)}\n")
                 f.close() 
         else:
             with open("Yanlis-Harfler.txt","a",encoding="utf-8") as f:
                 f.write(f"{i}\n")
                 f.close()
-
+        sira+=1
+    sira=0
     with open("Yanlis-Harfler.txt","r",encoding="utf-8") as f:
         icerik=f.readlines()
 
@@ -53,13 +55,33 @@ def cozum():
         for i in range(len(sozluk)):
             fh.write(sozluk[i])
 
+    yanlisYerler()
 
 def besHarfliler():            
-    with open("besharf.txt","r",encoding="utf-8") as f:
+    with open("besharfHafiza.txt","r",encoding="utf-8") as f:
         kelimeler = f.readlines()
         f.close()
     return kelimeler
 
+def yanlisYerler():
+    with open("Olan-Harfler.txt","r",encoding="utf-8") as f:
+        yerler=f.readlines()
+
+    sozluk=besHarfliler()
+    for i in range(len(yerler)):
+        silinecekler=[]
+        for j in sozluk:
+            if yerler[i][0]==j[i]:
+                silinecekler.append(sozluk.index(j))
+        if silinecekler!=[]:
+            silinecekler.reverse()
+        #print(silinecekler)
+            for j in silinecekler:
+                sozluk.pop(j)
+    
+    with open("besharfHafiza.txt","w",encoding="utf-8") as fh:
+        for i in range(len(sozluk)):
+            fh.write(sozluk[i])
       
 start()
 print('HOŞ GELDİNİZ'.center(50,'*'))
@@ -67,5 +89,3 @@ while True:
     print("Senin için en değerli kelimeleri buldum")
     deger.degerli()
     cozum()
-
-
