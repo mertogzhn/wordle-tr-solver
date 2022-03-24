aa77c0015663d878f5ba061d9f1e8e59727e7939
Mert Oğuzhanasiltürk <mert.oguzhanasilturk@gmail.com>
2022-03-24 21:13:05 +0300

    Giorgio by Moroder

--- wordle-solve.py
+++ wordle-solve.py
@@ -1,10 +1,41 @@
-from traceback import print_tb
-
-
+from re import M
 import enDegerliKelimeler as deger
+def start():
+    with open("Dogru-Yerde-Harfler.txt","w",encoding="utf-8") as f:
+        f.close()
+    with open("Olan-Harfler.txt","w",encoding="utf-8") as f:
+        f.close() 
+    with open("Yanlis-Harfler.txt","w",encoding="utf-8") as f:
+        f.close()
 
-while True:
-    print('HOŞ GELDİNİZ'.center(50,'*'))
-    print("Senin için en değerli kelimeleri buldum")
-    deger.degerli()
-    break
+def cozum():
+    girdi=input("girdiniz ne oldu: ")
+    print("Doğru yerdekiler için *\nYanlış yerdekiler için +\nOlmayanlar için -")
+
+    for i in girdi:
+        donus=input(f"{i} harfi geri dönüsü: ")
+        if donus=="*":
+            with open("Dogru-Yerde-Harfler.txt","a",encoding="utf-8") as f:
+                f.write(f"{i}\n")
+                f.close()
+        elif donus=="+":
+            with open("Olan-Harfler.txt","a",encoding="utf-8") as f:
+                f.write(f"{i}\n")
+                f.close() 
+        else:
+            with open("Yanlis-Harfler.txt","a",encoding="utf-8") as f:
+                f.write(f"{i}\n")
+                f.close()
+
+    with open("Yanlis-Harfler.txt","r",encoding="utf-8") as f:
+        icerik=f.readlines()
+
+    print(icerik)
+
+      
+start()
+print('HOŞ GELDİNİZ'.center(50,'*'))
+print("Senin için en değerli kelimeleri buldum")
+deger.degerli()
+cozum()
+
